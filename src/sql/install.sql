e48ce6365fc7adbecb42d2f27ddf889a195d87cc
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-12-12 11:45:33 +0000

    Chore: Release 1.11.2

--- src/sql/install.sql
+++ src/sql/install.sql
@@ -2,8 +2,8 @@
 
 -- This file is only needed for new installations.
 
-SET @DBVERSION="86";
-SET @VERSION="1.11.1";
+SET @DBVERSION="87";
+SET @VERSION="1.11.2";
 
 -- --------------------------------------------------------
 
